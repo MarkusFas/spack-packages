56b022862c476347a7cba828fc4a082299aae13c
Markus Fasching <MarkusFas7@hotmail.com>
2026-06-08 18:47:39 +0200

    fix dependencies

--- repos/spack_repo/builtin/packages/libmetatomic_torch/package.py
+++ repos/spack_repo/builtin/packages/libmetatomic_torch/package.py
@@ -32,7 +32,7 @@ class LibmetatomicTorch(CMakePackage):
     depends_on("cmake@3.16:", type="build")
     depends_on("cxx", type="build")
     depends_on("c", type="build")
-    depends_on("libmetatensor-torch@0.8.0:0.8", when="@0.1.4:")
+    depends_on("libmetatensor-torch@0.8.0:0.8", when="@0.1.4:0.1.12")
     depends_on("libmetatensor-torch@0.9.1", when="@0.1.13")
     depends_on("py-torch@2.3.0:", when="@0.1.9:")
     depends_on("py-torch@2.1.0:")
